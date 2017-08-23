a70dd3f7334b4e51632f93979214d9742813210b
Mads Kristensen <post@madskristensen.net>
2017-08-23 09:30:59 -0700

    Added invalid path analyzer

--- src/WebOptimizer.Analyzers/Descriptors.cs
+++ src/WebOptimizer.Analyzers/Descriptors.cs
@@ -16,6 +16,10 @@ namespace WebOptimizer.Analyzers
             "The content type is not a valid content type.",
             "\"{0}\" is not a valid content type.");
 
+        public static DiagnosticDescriptor InvalidPath => Generate(1003,
+            "Source file paths must not start with \"../\"",
+            "\"{0}\" is not a valid path. Don't use paths starting with \"../\" but instead set the UseContentRoot setting to true.");
+
         private static DiagnosticDescriptor Generate(int id, string title, string messageFormat, DiagnosticSeverity severity = DiagnosticSeverity.Warning)
         {
             return new DiagnosticDescriptor(
